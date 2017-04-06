341c494d8b55594a029d3dcfc0956f15c990cfd9
Michael McConville <mmcco@mykolab.com>
2017-04-05 19:03:00 -0600

    Add option parsing

--- pure-funcs.sh
+++ pure-funcs.sh
@@ -1,5 +1,28 @@
 set -e
 
+usage() {
+	echo "usage: pure-func (-l|-x)"
+	exit 255
+}
+
+# ensure that we get exactly one argument
+if [ -z "$1" ] || [ -n "$2" ]
+then
+	usage
+fi
+
+case $1 in
+	'-l')
+		echo "el"
+		;;
+	'-x')
+		echo "ex"
+		;;
+	*)
+		usage
+		;;
+esac
+
 find . -name \*.c -print0 \
 	| xargs -0 ctags -x \
 	| awk '{print $3, "\t", $2, "\t", $1}' \
